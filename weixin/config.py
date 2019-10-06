c371d6c27ce5391b4775d11b9d3c0f0bedee3dc1
xuefa <1870212598@qq.com>
2019-10-06 18:54:20 +0800

    配置文件

---
+++ weixin/config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# @Time    : 2019/10/6 18:50
+# @Author  : hxf
+# @Email   : 1870212598@qq.com
+# @File    : config.py
+# Description :
+# ----------------------------------
+
+# 设置全局变量
+KEYWORD='风景'
+PROXY_POOL_URL='http://127.0.0.1:5000/get'
+MONGO_URI ='localhost'
+MONGO_DB='weixin'
