a21fc47fdf4354c8ebda7f65f51139c2f43065ae
xuefa <1870212598@qq.com>
2019-10-09 08:18:55 +0800

    chengfakoujue

---
+++ pythonBase/pythonBase.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# @Time    : 2019/10/9 8:07
+# @Author  : hxf
+# @Email   : 1870212598@qq.com
+# @File    : pythonBase.py
+# Description :
+# ----------------------------------
+'''
+	python的基本语法知识
+'''
+
+'''
+乘法口诀
+	end="" 代表不换行输出
+'''
+for i in range(1,10):
+	for j in range(1,i+1):
+		print(str(i)+"*"+str(j)+"="+str(i*j),end=" ")
+	print()# 换行
